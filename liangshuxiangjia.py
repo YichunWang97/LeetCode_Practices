c927b4630c089e979cee9e80669588482488fd8a
YichunWang97 <76996855+YichunWang97@users.noreply.github.com>
2021-07-25 17:01:10 +0800

    Add files via upload

---
+++ liangshuxiangjia.py
@@ -0,0 +1,47 @@
+# 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+#
+# 请你将两个数相加，并以相同形式返回一个表示和的链表。
+#
+# 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+#
+# 示例 1：
+#
+# 输入：l1 = [2,4,3], l2 = [5,6,4]
+# 输出：[7,0,8]
+# 解释：342 + 465 = 807.
+
+# 示例 2：
+#
+# 输入：l1 = [0], l2 = [0]
+# 输出：[0]
+
+# 示例 3：
+#
+# 输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+# 输出：[8,9,9,9,0,0,0,1]
+
+l1 = [9, 9, 9, 9, 9, 9, 9]
+l2 = [9, 9, 9, 9]
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        dummy = p = ListNode(None)
+        s = 0
+
+        while l1 or l2 or s:
+            s += (l1.val if l1 else 0) + (l2.val if l2 else 0)
+            p.next = ListNode(s % 10)
+            p = p.next
+            s //= 10
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return dummy.next
