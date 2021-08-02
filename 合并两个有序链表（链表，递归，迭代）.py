051b33ea02a52d414c460e5e0664c3ecfc0b8d61
YichunWang97 <76996855+YichunWang97@users.noreply.github.com>
2021-08-02 21:09:07 +0800

    Create 合并两个有序链表（链表，递归，迭代）.py

---
+++ 合并两个有序链表（链表，递归，迭代）.py
@@ -0,0 +1,71 @@
+# 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+
+# 示例 1：
+
+# 输入：l1 = [1,2,4], l2 = [1,3,4]
+# 输出：[1,1,2,3,4,4]
+# 示例 2：
+
+# 输入：l1 = [], l2 = []
+# 输出：[]
+# 示例 3：
+
+# 输入：l1 = [], l2 = [0]
+# 输出：[0]
+
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode-cn.com/problems/merge-two-sorted-lists
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+##########################################################################################
+# 网络题解：
+# 第一种：递归方法
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if not l1: return l2  # 终止条件，直到两个链表都空
+        if not l2: return l1
+        if l1.val <= l2.val:  # 递归调用
+            l1.next = self.mergeTwoLists(l1.next,l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1,l2.next)
+            return l2
+
+# 作者：z1m
+# 链接：https://leetcode-cn.com/problems/merge-two-sorted-lists/solution/yi-kan-jiu-hui-yi-xie-jiu-fei-xiang-jie-di-gui-by-/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+##########################################################################################
+# 第二种：迭代方法
+
+        prehead = ListNode(None)
+
+        prev = prehead
+        while l1 and l2:
+            if l1.val <= l2.val:
+                prev.next = l1
+                l1 = l1.next
+            else:
+                prev.next = l2
+                l2 = l2.next            
+            prev = prev.next
+
+        # 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+        prev.next = l1 if l1 is not None else l2
+
+        return prehead.next
+
+# 作者：LeetCode-Solution
+# 链接：https://leetcode-cn.com/problems/merge-two-sorted-lists/solution/he-bing-liang-ge-you-xu-lian-biao-by-leetcode-solu/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
